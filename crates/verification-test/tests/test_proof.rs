e67d750b0ded0c931199f5f50814b1771852c149
youtpout <eddyboughioul@hotmail.fr>
2026-03-05 15:26:15 +0100

    check proof

--- crates/verification-test/tests/test_proof.rs
+++ crates/verification-test/tests/test_proof.rs
@@ -2,6 +2,7 @@
 // cargo test --test test_proof -- --nocapture
 
 use anyhow::Result;
+use ark_ff::Zero;
 use base64::{engine::general_purpose, Engine as _};
 use ledger::{
     proofs::{
@@ -14,11 +15,11 @@ use ledger::{
     },
     VerificationKey,
 };
+use mina_curves::pasta::Fq;
 use mina_p2p_messages::v2::{
     MinaBaseVerificationKeyWireStableV1, PicklesProofProofsVerified2ReprStableV2,
 };
 use verification_test::parse_o1js_proof_json;
-
 
 #[test]
 fn test_deserialize_o1js_proof() {
@@ -58,19 +59,9 @@ fn test_deserialize_o1js_proof() {
     let prover_proof = make_padded_proof_from_p2p(proof).expect("make_padded_proof");
     eprintln!("proof padded successfully");
 
-    let public_inputs = parsed
-        .public_input
-        .iter()
-        .map(|s| s.parse::<u128>().expect("parse public input"))
-        .collect::<Vec<_>>();
+    let mut public_inputs = vec![Fq::zero(); 40];
 
-    // At this point you have everything decoded:
-    // - parsed.proof           (PicklesProofProofsVerified2ReprStableV2)
-    // - parsed.public_input    (Vec<String> from o1js)
-    // - parsed.public_output   (Vec<String> from o1js)
-    // - verification_key       (VerificationKey)
-    // - verifier_index         (for Kimchi)
-    // - deferred_values        (Pickles deferred values)
+    let result = verify_with(&verifier_index, &prover_proof, &public_inputs);
 
-
+    assert!(result.is_ok(), "invalid proof: {:?}", result.err());
 }
